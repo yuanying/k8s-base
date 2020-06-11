ee0516e23af9e31d4315fb345af9f9e55cf9e8c4
OTSUKA, Yuanying <mototsuk@zlab.co.jp>
2020-06-11 23:08:04 +0000

    Encrypt single secret directory

--- bin/encrypt.sh
+++ bin/encrypt.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-set -eu
+set -e
 export LC_ALL=C
 
-for s in $(find . -maxdepth 2 -type d -name secrets); do
-    echo "Encrypt ${s} directory"
-    d=$(dirname ${s})
-    tar cvf ${d}/secrets.tar ${s}
-    gpg --yes --batch --passphrase-fd 0 -r yuanying@fraction.jp -ea ${d}/secrets.tar < password.txt
-done
+: "${TARGET_DIR:=$1}"
+
+[[ -z "${TARGET_DIR}" ]] && { echo "TARGET_DIR is requred."; exit 1; }
+[[ ! -d "${TARGET_DIR}/secrets" ]] && { echo "TARGET_DIR/secrets should be directory"; exit 1; }
+
+s=${TARGET_DIR}/secrets
+
+echo "Encrypt ${s} directory"
+d=$(dirname ${s})
+tar cvf ${d}/secrets.tar ${s}
+gpg --yes --batch --passphrase-fd 0 -r yuanying@fraction.jp -ea ${d}/secrets.tar < password.txt
